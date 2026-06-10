cf28096b723a5f76f0281b875a324ff76d81bcf2
aymanabdeenn <aymanabdeen03@gmail.com>
2026-06-10 14:52:25 +0300

    Initial commit

---
+++ config-server/src/test/java/com/ayman/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
