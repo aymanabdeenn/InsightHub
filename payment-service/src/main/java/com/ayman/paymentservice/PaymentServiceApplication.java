8599259825db1016ae64649e1a2097f2870a9edf
aymanabdeenn <aymanabdeen03@gmail.com>
2026-06-18 15:36:15 +0300

    feat: Added the config-lib module and created the GlobalExceptionHandler.java

--- payment-service/src/main/java/com/ayman/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/ayman/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.ayman.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ayman.paymentservice", "com.ayman.configlib.errorhandler"})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
