794cdb7f3ea1ed68e028486b4ac836c0a1181146
aymanabdeenn <aymanabdeen03@gmail.com>
2026-06-24 21:08:45 +0300

    fix: Fixed the module inclusion to other services like payment-service and gateway.

--- payment-service/src/main/java/com/ayman/paymentservice/payment/PaymentController.java
+++ payment-service/src/main/java/com/ayman/paymentservice/payment/PaymentController.java
@@ -1,5 +1,6 @@
 package com.ayman.paymentservice.payment;
 
+import com.ayman.configlib.error.ApiException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,11 @@ public class PaymentController {
     @GetMapping
     public ResponseEntity<?> sendResponse() {
         return ResponseEntity.ok("Hello");
+    }
+
+    @GetMapping("/test-error")
+    public ResponseEntity<?> triggerError() {
+        throw new ApiException("INTERNAL_ERROR");
     }
 
 }
