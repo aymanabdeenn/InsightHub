a64de20371d3f1d6a12fae290d487e2fa419275f
aymanabdeenn <aymanabdeen03@gmail.com>
2026-07-15 14:22:35 +0300

    fix: changed the json property of PlainConnectionConfig in DataSourceModDTO.java

--- data-source-service/src/main/java/com/ayman/datasourceservice/domain/dto/DataSourceModDTO.java
+++ data-source-service/src/main/java/com/ayman/datasourceservice/domain/dto/DataSourceModDTO.java
@@ -1,6 +1,7 @@
 package com.ayman.datasourceservice.domain.dto;
 
 import com.ayman.datasourceservice.domain.PlainConnectionConfig;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record DataSourceModDTO(String name, PlainConnectionConfig config) {
+public record DataSourceModDTO(String name, @JsonProperty("connectionConfig") PlainConnectionConfig config) {
 }
